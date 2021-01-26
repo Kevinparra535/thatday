68cf185043e1605bba7dc74855d454fb61f8d561
Kevin Parra <53342165+Kevinparra535@users.noreply.github.com>
2021-01-26 18:30:55 -0500

    [ACTUALIZADO] Landing Page

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -1,20 +1,24 @@
 const key = "G5V8-ODN7-A1KD-S8DK";
-const limit = "&results=6";
+const limit = "&results=10";
 const endPoint = "&seed=huskiesarecute&page=1";
 $.ajax({
   url: `https://randomuser.me/api/?${key}${limit}${endPoint}`,
   dataType: "json",
   success: function (data) {
     let card = data.results.map(
-      (user) => `<div class="testimoniesCards">
-    <div class="testimoniesCards__user">
-      <img src="${user.picture.medium}" alt="user profile" />
-    </div>
-    <div class="testimoniesCards__text">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. ✨
-    </div>
-  </div>`
+      (user) => `
+      <div class="swiper-slide">
+        <div class="testimoniesCards">
+          <div class="testimoniesCards__user">
+            <img src="${user.picture.medium}" alt="user profile" />
+          </div>
+          <div class="testimoniesCards__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. ✨
+          </div>
+        </div>
+      </div>
+      `
     );
-    document.querySelector(".about__testimonies").innerHTML = card;
+    document.querySelector(".swiper-wrapper").innerHTML = card;
   },
 });
